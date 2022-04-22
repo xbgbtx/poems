95edc16e4ed2b30c76c1b1d0611012414e90aa7a
xbgbtx <you@example.com>
2022-04-22 16:44:47 +0100

    Read and print file

--- poem_split.py
+++ poem_split.py
@@ -1,12 +1,17 @@
 class PoemReader:
     def __init__(self):
-        print("baz")
+        with open("all.txt", "r") as f:
+            self.lines = f.readlines()
+
+    def dump(self):
+        for l in self.lines:
+            print(l)
+
 
 def main():
-    print("bar")
 
     pr = PoemReader()
+    pr.dump()
 
 if __name__ == "__main__":
-    print("foo")
     main()
